47f64d1f6a23969ffacb0d368e8c4bbb5894086d
unknown <sean.herbison@gmail.com>
2023-04-04 13:20:30 -0700

    Finished setup steps.

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -107,6 +107,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from local_settings import *
+    from heroes_villains_project.local_settings import *
 except ImportError:
     pass
