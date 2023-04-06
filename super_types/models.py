eee6e0cfc955cb993b639d57ccbc208e1de1dcc6
unknown <sean.herbison@gmail.com>
2023-04-06 11:22:32 -0700

    Made Super serializer, SuperType and Super models. Added initial GET view for super by id.

--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SuperType(models.Model):
+    super_type = models.CharField(max_length=255)
+    # Directions are unclear, and "type" is a reserved keyword
+
+#As a developer, I want to create a SuperType model in a “super_types” app. 
+#Property names must be in snake_case and match the following exactly! 
+#   type – CharField 
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Super
+from rest_framework.decorators import api_view
+from .serializers import SuperSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
